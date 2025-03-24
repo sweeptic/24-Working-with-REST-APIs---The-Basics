e4e309edf5e3917c7044259455445339b55844f6
Attila Horvath <sweeptic@gmail.com>
2025-03-24 20:42:13 +0100

    008 REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 
 app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // GET /feed/posts
 app.use('/feed', feedRoutes);
--- controllers/feed.js
+++ controllers/feed.js
@@ -6,7 +6,12 @@ export function createPost(req, res, next) {
   const title = req.body.title;
   const content = req.body.content;
 
-  res
-    .status(201)
-    .json({ message: 'Post created successfully', post: { id: new Date().toISOString(), title, content } });
+  res.status(201).json({
+    message: 'Post created successfully',
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content,
+    },
+  });
 }
